775caf4f83f05c288df3b03c3e9ccf46dc31b0b7
HQ <leehyeonkyu@naver.com>
2024-04-09 20:18:47 +0900

    README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 실험 버전
 
-gymnasium == 0.29.1
+<!-- gym == 0.26.2 -->
+gymnasium==0.29.1
+readchar==4.0.6
+colorama==0.4.6
--- test_frozenlake_v3.py
+++ test_frozenlake_v3.py
@@ -1,15 +1,17 @@
-
 import gymnasium
 from gymnasium.envs.registration import register
-import readchar 
+import readchar
 import colorama as cr
 
 
 cr.init(autoreset=True)
-gymnasium.envs.registration.register(id='FrozenLake-v3',
-entry_point='gymnasium.envs.toy_text:FrozenLakeEnv', kwargs={'map_name':'4x4', 'is_slippery':False})
+gymnasium.envs.registration.register(
+    id="FrozenLake-v3",
+    entry_point="gymnasium.envs.toy_text:FrozenLakeEnv",
+    kwargs={"map_name": "4x4", "is_slippery": False},
+)
 
-env = gymnasium.make('FrozenLake-v3', render_mode='ansi')
+env = gymnasium.make("FrozenLake-v3", render_mode="ansi")
 env.reset()
 print(env.render())
 
@@ -18,12 +20,7 @@ DOWN = 1
 RIGHT = 2
 UP = 3
 
-arrow_keys = {
-    '\x1b[A' : UP,
-    '\x1b[B' : DOWN,
-    '\x1b[C' : RIGHT,
-    '\x1b[D' : LEFT
-}
+arrow_keys = {"\x1b[A": UP, "\x1b[B": DOWN, "\x1b[C": RIGHT, "\x1b[D": LEFT}
 
 
 while True:
@@ -35,8 +32,7 @@ while True:
     observation, reward, terminated, truncated, info = env.step(action)
     print(env.render())
     print(f"State: {observation} Action: {action} Reward: {reward} Info: {info} ")
-    
+
     if terminated:
         print("Finished with reward", reward)
         break
-    

7e1b6db10d51697139bacca74243674a23578db9
HQ <leehyeonkyu@naver.com>
2024-04-26 15:45:11 +0900

    break

--- cdqn.py
+++ cdqn.py
@@ -56,3 +56,42 @@ class ReplayMemory:
 
     def __len__(self):
         return len(self.memory)
+
+
+class Agent:
+    def __init__(self, N):
+        self.q = Network(N)
+        self.target = Network(N)
+        self.update_target()
+        self.V_min = -10
+        self.V_max = 10
+        self.gamma = 0.99
+        self.delta_z = (self.V_max - self.V_min) / (N - 1)
+        self.optimizer = torch.optim.Adam(self.q.parameters(), lr=0.001)
+        self.N = N
+        self.target_update_frequency = 10
+
+    def action(self, state, epsilon):
+        if np.random.randn() < epsilon:
+            return np.random.randint(0, 2)
+
+        else:
+            z_distribution = torch.from_numpy(
+                np.array([[self.V_min + i * self.delta_z for i in range(self.N)]])
+            )
+            z_distribution = torch.unsqueeze(z_distribution, 2).float()
+
+            Q_dist, _ = self.q.forward(state)
+            Q_dist = Q_dist.detach()
+            Q_target = torch.matmul(Q_dist, z_distribution)
+
+            return Q_target.argmax(dim=1)[0].detach().cpu().numpy()[0]
+
+        def update_target(self):
+            self.target.load_state_dict(self.q.state_dict())
+
+        def update(self, buffer):
+            if len(buffer) < buffer.batch_size:
+                return
+
+            batch = buffer
